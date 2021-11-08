f282b22ac5a75d56f3e7debdf588a0010250a1a5
R2Boyo25 <r2boyo25@gmail.com>
2021-11-08 01:29:51 -0600

    remove dots from extensions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     files = []
     for root, files, dir in os.walk("."):
         for file in files:
-            if file.split(".")[-1].lower() in [".cpp", ".c"]:
+            if file.split(".")[-1].lower() in ["cpp", "c"]:
                 files.append(f"{root}/{file}".replace("//", "/"))
 
     filestr = " ".join( [ f"\"{i}\"" for i in files ] )
