5f69c913af4e5d02cbf820f2ebde8f96f88f546f
R2boyo25 <r2boyo25@gmail.com>
2022-01-30 16:54:30 -0600

    Fix compile script

--- .avalon/comp.py
+++ .avalon/comp.py
@@ -1,5 +1,7 @@
 import os, sys
 
-if os.system("mortar > /dev/null 2>&1"):
+print("Checking for mortar")
+if os.system("mortar"):
+    print("Well, I didn't find it so I'll compile it with g++")
     if os.system("g++ -Ofast -omortar *.cpp include/SHA1/sha1.cpp -std=c++17 -lpthread -Iinclude"):
         sys.exit(1)
